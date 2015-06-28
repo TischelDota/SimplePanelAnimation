ea8adb5a95ad59f828d6ba8b748faedc22989912
TischelDota <tischeldota@gmail.com>
2015-06-27 22:36:19 -0300

    Transition needs to be set before updating the values (this gets read of the awful hack) Fixed bug with precision errors

--- simple_panel_animation.js
+++ simple_panel_animation.js
@@ -21,30 +21,24 @@ var DEFAULT_EASE = "linear";
 function AnimatePanel(panel, values, duration, ease, delay)
 {
 	// generate transition string
-	var durationString = (duration != null ? duration * 1000 + ".0ms" : DEFAULT_DURATION);
+	var durationString = (duration != null ? parseInt(duration * 1000) + ".0ms" : DEFAULT_DURATION);
 	var easeString = (ease != null ? ease : DEFAULT_EASE);
-	var delayString = (delay != null ? delay * 1000 + ".0ms" : "0.0ms"); 
+	var delayString = (delay != null ? parseInt(delay * 1000) + ".0ms" : "0.0ms"); 
 	var transitionString = durationString + " " + easeString + " " + delayString;
-
-	// HACK +
-	// For some reason I gotta set a dummy transition first, with just the duration
-	// oterwise the first animation ever will be instantaneous (WHY?)
-	panel.style.transition = "transform " + durationString + " ease 0s;";
-	// -
 
 	var i = 0;
 	var finalTransition = ""
 	for (var property in values)
 	{
-		// apply values
-		panel.style[property] = values[property];
-
 		// add property to transition
 		finalTransition = finalTransition + (i > 0 ? ", " : "") + property + " " + transitionString;
-
 		i++;
 	}
 
 	// apply transition
 	panel.style.transition = finalTransition + ";";
+
+	// apply values
+	for (var property in values)
+		panel.style[property] = values[property];
 }
